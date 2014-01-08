4301f730c82e11b13f75da65d6938dca70ce7bbf
gsorita <gsorita@rollbase.ph>
2014-01-08 15:13:42 +0800

    Fundamental Report

--- Trigger/Update Bond, Cash, Equity.js
+++ Trigger/Update Bond, Cash, Equity.js
@@ -12,7 +12,7 @@ var objId = "{!id}"
 
 /* -- BONDS -- */
 
-var bonds = parseFloat(setToZero("{!Bonds_Europe__c#value}")) + parseFloat(setToZero("{!Bonds_Europe_Gov__c#value}")) + parseFloat(setToZero("{!Bonds_Europe_Other__c#value}")) + parseFloat(setToZero("{!Bonds_Hong_Kong__c#value}")) + parseFloat(setToZero("{!Bonds_Hong_Kong_Gov__c#value}")) + parseFloat(setToZero("{!Bonds_Hong_Kong_Other__c#value}")) + parseFloat(setToZero("{!Bonds_Japan__c#value}")) + parseFloat(setToZero("{!Bonds_Japan_Gov__c#value}")) + parseFloat(setToZero("{!Bonds_Japan_Other__c#value}")) + parseFloat(setToZero("{!Bonds_Other__c#value}")) + parseFloat(setToZero("{!Bonds_Other_Gov__c#value}")) + parseFloat(setToZero("{!Bonds_Other_Other__c#value}")) + parseFloat(setToZero("{!Bonds_US__c#value}")) + parseFloat(setToZero("{!Bonds_US_Gov__c#value}")) + parseFloat(setToZero("{!Bonds_US_Other__c#value}"));
+var bonds = parseFloat(setToZero("{!Bonds_Greater_China__c#value}")) + parseFloat(setToZero("{!Bonds_Asia__c#value}")) + parseFloat(setToZero("{!Bonds_Australia_New_Zealand__c#value}")) + parseFloat(setToZero("{!Bonds_Japan__c#value}")) + parseFloat(setToZero("{!Bonds_Europe__c#value}")) + parseFloat(setToZero("{!Bonds_North_America__c#value}")) + + parseFloat(setToZero("{!Bonds_Other__c#value}"));
 
 bonds = bonds.toFixed(2);
 bonds = bonds.toString();
@@ -21,7 +21,7 @@ rbv_api.println("bonds: " + bonds);
 
 /* -- CASH -- */
 
-var cash = parseFloat(setToZero("{!Cash_Cash__c#value}")) + parseFloat(setToZero("{!Cash_Term_Deposits__c#value}"));
+var cash = parseFloat(setToZero("{!Cash__c#value}")) + parseFloat(setToZero("{!Money_Markets__c#value}")) + parseFloat(setToZero("{!Cash_Equivalents_Other__c#value}"));
 cash = cash.toFixed(2);
 cash = cash.toString();
 
@@ -29,13 +29,17 @@ rbv_api.println("cash: " + cash);
 
 /* -- EQUITY -- */
 
-var equity = parseFloat(setToZero("{!Equity_Asia__c#value}")) + parseFloat(setToZero("{!Equity_Asia_Asia_Other__c#value}")) + parseFloat(setToZero("{!Equity_Asia_Global__c#value}")) + parseFloat(setToZero("{!Equity_Asia_Japan__c#value}")) + parseFloat(setToZero("{!Equity_Global_Europe__c#value}")) + parseFloat(setToZero("{!Equity_Global_North_America__c#value}")) + parseFloat(setToZero("{!Equity_Global_Other__c#value}")) + parseFloat(setToZero("{!Equity_Greater_China__c#value}")) + parseFloat(setToZero("{!Equity_Greater_China_China__c#value}")) + parseFloat(setToZero("{!Equity_Greater_China_Hong_Kong__c#value}")) + parseFloat(setToZero("{!Equity_Greater_China_Taiwan__c#value}"));
+var equity = parseFloat(setToZero("{!Equity_Greater_China__c#value}")) + parseFloat(setToZero("{!Equity_Asia__c#value}")) + parseFloat(setToZero("{!Equity_Australia_New_Zealand__c#value}")) + parseFloat(setToZero("{!Equity_Asia_Japan__c#value}")) + parseFloat(setToZero("{!Equity_Europe__c#value}")) + parseFloat(setToZero("{!Equity_North_America__c#value}")) + parseFloat(setToZero("{!Equity_Other__c#value}"));
 
 equity = equity.toFixed(2);
 equity = equity.toString();
 
 rbv_api.println("equity: " + equity);
 
+var total = (parseFloat(bonds) + parseFloat(cash) + parseFloat(equity)).toFixed(2).toString();
+rbv_api.println("total: " + total);
+
 rbv_api.setFieldValue(objName, objId, "Bonds__c", bonds);
-rbv_api.setFieldValue(objName, objId, "Cash__c", cash);
+rbv_api.setFieldValue(objName, objId, "Cash_and_Equivalents__c", cash);
 rbv_api.setFieldValue(objName, objId, "Equity__c", equity);
+rbv_api.setFieldValue(objName, objId, "Total_Fund__c", total);
