06359449b4e5affa1fe862130658d95af9cfa085
Godfrey Sorita <gsorita@rollbase.ph>
2014-04-02 11:24:42 +0800

    2114-03-28 changes

--- Formula/Attach Earnings to Index [After Create & Update].js
+++ Formula/Attach Earnings to Index [After Create & Update].js
@@ -1,15 +1,26 @@
 function main () { 
-	rbv_api.setFieldValue("funds_detail__c", parseInt('{!id}'), 'R1843405', '');
 	var fund_id = parseInt("{!Pfunds_detail__c#id}");
-	var effective_date_iso = '{!R182293.effective_date#iso}';
-	var effect_date_5_iso = '{!R182293.effective_date__minus_5_years_#iso} 23:59:59';
-	var start_date = '';
-	var arr = new Array();
+	rbv_api.setFieldValue("funds_detail__c", parseInt('{!id}'), 'R1843405', '');
 	
+	var arr = new Array(),
+		startDate_iso = '{!Date_Opened__c#iso}',
+		effective_date_iso = '{!R182293.effective_date#iso}',
+		effective_date_5_iso = '{!R182293.effective_date__minus_5_years_#iso} 23:59:59',
+		startDate = new Date('{!Date_Opened__c}'),
+		strMinDate = "#CALC_MIN.R182263( date3__c | date3__c )",
+		minDate = new Date(strMinDate.substr(0,4), strMinDate.substr(5,2), strMinDate.substr(8,2)),
+		effective_date_5 = new Date("{!R182293.effective_date__minus_5_years_}");
 	var query = "SELECT id FROM funds_earning__c WHERE R182254 = ? AND Date2__c <= ? AND Date2__c > ?";
+	rbv_api.println(minDate +"::"+ effective_date_5); 
 	
-	var earning = rbv_api.selectQuery(query, 1000, fund_id, effective_date_iso, effect_date_5_iso);
-	rbv_api.print(earning.length);
+	if (startDate < effective_date_5) {
+		rbv_api.println(startDate + "<" + effective_date_5);
+		earning = rbv_api.selectQuery(query, 1000, fund_id, effective_date_iso, effective_date_5_iso);
+	} else {
+		rbv_api.println(effective_date_5 + "<" + startDate);
+		earning = rbv_api.selectQuery(query, 1000, fund_id, effective_date_iso, startDate_iso);
+	}	
+	rbv_api.print("length:" + earning.length);
 	
 	count = 0;
 	while (count < earning.length) {
