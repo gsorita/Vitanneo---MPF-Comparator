569222e898dcf6d208d17de49b0aab4981cca396
Godfrey Sorita <gsorita@rollbase.ph>
2014-05-05 11:07:36 +0800

    Revert from latest

--- Formula/Attach Earning to Duplicate.js
+++ Formula/Attach Earning to Duplicate.js
@@ -3,21 +3,18 @@ function main () {
 	rbv_api.setFieldValue("funds_detail__c", fund_id, 'R1831314', ''); //clear value of fund earning duplicate
 	
 	var arr = new Array(),
-		startDate_iso = '{!Date_Opened__c#iso}',
 		effective_date_iso = '{!R182293.effective_date#iso}',
 		effective_date_5_iso = '{!R182293.effective_date__minus_5_years_#iso} 23:59:59',
-		startDate = new Date('{!Date_Opened__c}'),
 		strMinDate = "#CALC_MIN.R182263( date3__c | date3__c )",
 		minDate = new Date(strMinDate.substr(0,4), strMinDate.substr(5,2), strMinDate.substr(8,2)),
 		effective_date_5 = new Date("{!R182293.effective_date__minus_5_years_}");
-	var query = "SELECT id, Date2__c FROM funds_earning__c WHERE R182254 = ? AND Date2__c <= ? AND Date2__c > ?";
+	var query = "SELECT id, Date2__c FROM funds_earning__c WHERE R182254 = ? AND Date2__c <= ? AND Date2__c > ?";	
+	rbv_api.println(minDate +"::"+ effective_date_5); 
 	
-	if (startDate < effective_date_5) {
-		rbv_api.println(startDate + "<" + effective_date_5);
+	if (minDate < effective_date_5) {
 		earning = rbv_api.selectQuery(query, 1000, fund_id, effective_date_iso, effective_date_5_iso);
 	} else {
-		rbv_api.println(effective_date_5 + "<" + startDate);
-		earning = rbv_api.selectQuery(query, 1000, fund_id, effective_date_iso, startDate_iso);
+		earning = rbv_api.selectQuery(query, 1000, fund_id, effective_date_iso, strMinDate);
 	}
 	
 	rbv_api.println("length:" + earning.length);
--- Formula/Chart Data X.js
+++ Formula/Chart Data X.js
@@ -1,60 +1,34 @@
-var end_date = new Date('{!R182293.effective_date}'),
-	start_date = new Date('{!Date_Opened__c}');
-
 ret = "";
-ret1 = new Array();
-ret2 = new Array();
+
 function main () {
-	var strMinDate = "#CALC_MIN.R182263( date3__c | date3__c )",
-		minDate = new Date(strMinDate.substr(0,4), strMinDate.substr(5,2), strMinDate.substr(8,2)),
-		effective_date_5 = new Date("{!R182293.effective_date__minus_5_years_}");
-  
-	"{!#LOOP_BEGIN.R1831314#636454}"
-	p("{!R182254.Date2__c}");
-	"{!#LOOP_END.R1831314}"
-	
-	var xinit = 0;
-			
-	if (ret1.length == 0) {
-		retString = ret1.toString();
-	} else {
-		rbv_api.println("length:" + ret2.length);
-		if (start_date < effective_date_5) {
-			rbv_api.println("start date:" + effective_date_5);
-			ret2[xinit] = getRet(effective_date_5);
-		} else {
-			rbv_api.println("start date:" + start_date);
-			ret2[xinit] = getRet(start_date);
-		}
-
-		if (ret1.length == 1) {
-			var xlast = ret1.length;
-		} else {
-			var xlast = ret1.length;
-			var xmid = Math.round(ret1.length / 2); 
-			rbv_api.println("xmid:" + xmid);
-			ret2[xmid] = ret1[xmid];
-		}
-		rbv_api.println("xlast:" + xlast);
-		ret2[xlast] = getRet(end_date);
-	}
-	 
-	retString = ret2.toString();
-	
-	retString = retString.substring(0, retString.length - 1);
-	return retString.replace(/\,/g, "|");
-}
-
-function p(myDate) {
-	ret1.push(getRet(myDate));
+  ret1 = new Array();
+  ret2 = new Array();
+  {!#LOOP_BEGIN.R182254#636454}
+	curdate = new Date("{!R182254.Date2__c}");
+	ret1.push(getRet(curdate));
 	ret2.push("");
+  {!#LOOP_END.R182254}
+  
+  
+  
+  if (ret1.length <= 3) {
+	retString = ret1.toString();
+  } else {
+	var xinit = 0, xmid = Math.round(ret1.length / 2), xlast = ret1.length - 1;
+	ret2[xinit] = ret1[xinit];
+	ret2[xmid] = ret1[xmid];
+	ret2[xlast] =  ret1[xlast];	  
+	retString = ret2.toString();
+  }
+  
+  retString = retString.substring(0, retString.length - 1);
+  return retString.replace(/\,/g, "|");
 }
 
 function getRet(myDate) {
-	myDate = new Date(myDate);
 	var curyear = myDate.getFullYear().toString();
 	var yr = curyear.substring(curyear.length, 2);
-	return getMonthNum(myDate.getMonth()+1) + "/" + yr + "|";
+	return getMonthNum(curdate.getMonth()+1) + "/" + yr + "|";
 }
 
 function getMonthNum (month) {
